bd114b044192c61ce36a474fce5d23379d1fd110
jelipo <me@jelipo.com>
2021-09-20 01:29:09 +0800

    udpate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
+use std::io::Read;
+
+fn main() -> anyhow::Result<()> {
     println!("Hello, world!");
+    let mut string = String::new();
+    let _ = reqwest::blocking::get("http://www.baidu.com/")?.read_to_string(&mut string)?;
+    println!("{}", string);
+    Ok(())
 }
