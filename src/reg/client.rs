c0fab36fdf11599c27c6ed61885d987f4c2a8bac
jelipo <me@jelipo.com>
2021-10-22 19:27:51 +0800

    update

--- src/reg/client.rs
+++ src/reg/client.rs
@@ -1,13 +1,17 @@
 use std::io::Read;
+use std::option::Option::Some;
 use std::time::Duration;
 
 use anyhow::{Error, Result};
 use log::{debug, warn};
 use reqwest::blocking::{Client, Request};
+use reqwest::header::{HeaderMap, HeaderValue};
 use reqwest::redirect::Policy;
 use reqwest::{Method, Url};
 use serde::de::DeserializeOwned;
 use serde::Serialize;
+
+const DOCKER_JSON_CONTENT_TYPE: &str = "application/vnd.docker.distribution.manifest.v2+json";
 
 #[derive(Clone)]
 pub struct HttpClient {
@@ -41,7 +45,7 @@ impl HttpClient {
         method: Method,
         body: Option<&T>,
     ) -> Result<R> {
-        return self.do_request::<T, R>(path, method, body)?;
+        return self.do_request::<T, R>(path, method, body);
     }
 
     fn do_request<T: Serialize + ?Sized, R: DeserializeOwned>(
@@ -51,13 +55,29 @@ impl HttpClient {
         body: Option<&T>,
     ) -> Result<R> {
         let request = self.build_request(path, method, body)?;
-        let response = self.client.execute(request)?;
+        let mut response = self.client.execute(request)?;
         let status_code = response.status();
-        if status_code.is_success() {
-            return Ok(response.json::<R>()?);
-        }
-        let result = response.read_to_string(&mut String::default());
-        debug!("")
+        let headers = response.headers();
+        return if status_code.is_success() {
+            Ok(response.json::<R>()?)
+        } else {
+            match get_content_type(headers) {
+                None => Err(Error::msg(format!(
+                    "Request to registry failed,status_code:{}",
+                    status_code.as_str()
+                ))),
+                Some(content_type) => {
+                    let mut body = String::default();
+                    let i = response.read_to_string(&mut body)?;
+                    Err(Error::msg(format!(
+                        "Request to registry failed,status_code:{} ,content-type:{} ,body:{}",
+                        status_code.as_str(),
+                        content_type,
+                        body,
+                    )))
+                }
+            }
+        };
     }
 
     fn build_request<T: Serialize + ?Sized>(
@@ -72,8 +92,30 @@ impl HttpClient {
             .request(method, url)
             .basic_auth(&self.username, Some(&self.password));
         if let Some(body_o) = body {
-            builder = builder.json(body_o)
+            builder = builder.json::<T>(body_o)
         }
         Ok(builder.build()?)
     }
+}
+
+fn get_content_type(headers: &HeaderMap<HeaderValue>) -> Option<&str> {
+    return if let Some(Ok(content_type)) = headers.get("content-type").map(|value| value.to_str()) {
+        Some(content_type)
+    } else {
+        None
+    };
+}
+
+pub enum ContentType {
+    /// application/vnd.docker.distribution.manifest.v2+json
+    ApplicationVndDockerDistributionManifestV2Json,
+}
+
+pub struct RegistryErr {
+    errors: Vec<SingleRegistryError>,
+}
+
+pub struct SingleRegistryError {
+    code: String,
+    message: String,
 }
