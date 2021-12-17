908326dfaa3130999ee956c44118fe6cd4a1e272
jelipo <me@jelipo.com>
2021-12-18 01:21:35 +0800

    update

--- src/config/cmd.rs
+++ src/config/cmd.rs
@@ -1,5 +1,6 @@
 use std::str::FromStr;
 
+use anyhow::Error;
 use clap::{ArgEnum, Parser, Subcommand};
 
 #[derive(Parser)]
@@ -11,20 +12,44 @@ pub enum CmdArgs {
 #[derive(clap::Args)]
 #[clap(about, author, version)]
 pub struct BuildArgs {
-    #[clap(subcommand)]
-    from: FromType,
+    #[clap(long, short)]
+    source: SourceType,
 }
 
-#[derive(Subcommand)]
-#[clap(about, version, author)]
-pub enum FromType {
-    FromDockerfile(DockerfileArgs)
+impl FromStr for SourceType {
+    type Err = anyhow::Error;
+
+    fn from_str(arg: &str) -> Result<Self, Self::Err> {
+        let potion = arg.chars().position(|c| c == ':')
+            .ok_or(Error::msg("error source"))?;
+        let source_type = &arg[..potion];
+        Ok(match source_type {
+            "dockerfile" => SourceType::Dockerfile(arg[potion + 1..].to_string()),
+            _ => return Err(Error::msg(format!("unknown source type: {}", source_type)))
+        })
+    }
+}
+
+pub enum SourceType {
+    Dockerfile(String)
 }
 
 #[derive(clap::Args)]
 #[clap(about, author, version)]
 pub struct DockerfileArgs {
     path: String,
+}
+
+#[derive(Subcommand)]
+#[clap(about, version, author)]
+pub enum ToType {
+    Registry(RegistryArgs)
+}
+
+#[derive(clap::Args)]
+#[clap(about, author, version)]
+pub struct RegistryArgs {
+    tag: String,
 }
 
 
