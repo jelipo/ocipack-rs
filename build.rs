3666de2dd579f14343a6d1cd8748fc3347aa9f4c
jelipo <me@jelipo.com>
2022-08-01 01:13:38 +0800

    add grpc

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/images.proto")?;
+    tonic_build::configure()
+        .compile(&["api/services/images/v1/images.proto"], &["api"])
+        .unwrap();
     Ok(())
 }
--- src/adapter/containerd.rs
+++ src/adapter/containerd.rs
@@ -2,11 +2,27 @@ pub struct ContainerdAdapter {}
 
 impl ContainerdAdapter {
     pub fn import_image(namespace: &str) {
-
+        let connect1 = containerd::services::images::v1::images_client::ImagesClient::connect("");
     }
 }
 
-pub mod hello_world {
-    tonic::include_proto!("containerd.services.images.v1");
+pub mod containerd {
+    pub mod services {
+        pub mod images {
+            pub mod v1 {
+                tonic::include_proto!("containerd.services.images.v1");
+            }
+        }
+    }
+
+    pub mod types {
+        tonic::include_proto!("containerd.types");
+    }
 }
 
+
+pub mod google {
+    pub mod protobuf {
+        tonic::include_proto!("google.protobuf");
+    }
+}
